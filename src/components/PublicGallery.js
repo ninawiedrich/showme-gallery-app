048accc7307c0ac4a62421a30d3902b46432c5e4
Nina Wiedrich <94755570+ninawiedrich@users.noreply.github.com>
2023-12-18 22:54:30 +0100

    fixed modal display issues, restored missing user profile features and filter options

--- src/components/PublicGallery.js
+++ src/components/PublicGallery.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Modal } from 'react-bootstrap';
+import { Container, Modal, Form, Row, Col } from 'react-bootstrap';
 import { db } from '../firebase-config';
 import { collection, query, where, onSnapshot, doc, getDoc } from 'firebase/firestore';
 import Carousel from 'react-multi-carousel';
@@ -9,6 +9,9 @@ import './PublicGallery.css';
 const PublicGallery = () => {
     const [sharedPhotos, setSharedPhotos] = useState([]);
     const [usernames, setUsernames] = useState({});
+    const [filterCategory, setFilterCategory] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filterUsername, setFilterUsername] = useState('');
     const [showBioModal, setShowBioModal] = useState(false);
     const [selectedUser, setSelectedUser] = useState(null);
     const [showImageModal, setShowImageModal] = useState(false);
@@ -38,7 +41,9 @@ const PublicGallery = () => {
         });
     }, []);
 
-    const handleUserNameClick = async (userId) => {
+    const handleUserNameClick = async (userId, e) => {
+      e.stopPropagation();
+
         const userDocRef = doc(db, 'users', userId);
         const userDocSnap = await getDoc(userDocRef);
         if (userDocSnap.exists()) {
@@ -52,6 +57,15 @@ const PublicGallery = () => {
         setShowImageModal(true);
     };
 
+    const applyFilters = photo => {
+        const matchesCategory = filterCategory ? photo.category === filterCategory : true;
+        const matchesTag = searchTerm ? photo.tag.toLowerCase().includes(searchTerm.toLowerCase()) : true;
+        const matchesUsername = filterUsername ? usernames[photo.userId]?.toLowerCase().includes(filterUsername.toLowerCase()) : true;
+        return matchesCategory && matchesTag && matchesUsername;
+    };
+
+    const filteredPhotos = sharedPhotos.filter(applyFilters);
+
     const responsive = {
         desktop: {
             breakpoint: { max: 3000, min: 1024 },
@@ -59,11 +73,11 @@ const PublicGallery = () => {
         },
         tablet: {
             breakpoint: { max: 1024, min: 464 },
-            items: 3
+            items: 3 
         },
         mobile: {
             breakpoint: { max: 464, min: 0 },
-            items: 1
+            items: 1 
         }
     };
 
@@ -72,6 +86,37 @@ const PublicGallery = () => {
             <div className="gallery-header">
                 <h1>Public Gallery</h1>
             </div>
+            <Row className="gallery-filter-row">
+                <Col>
+                    <Form.Control
+                        as="select"
+                        value={filterCategory}
+                        onChange={(e) => setFilterCategory(e.target.value)}
+                    >
+                        <option value="">All Categories</option>
+                        <option value="Nature">Nature</option>
+                        <option value="Animals">Animals</option>
+                        <option value="People">People</option>
+                        <option value="Technology">Technology</option>
+                    </Form.Control>
+                </Col>
+                <Col>
+                    <Form.Control
+                        type="text"
+                        placeholder="Search by tag"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                </Col>
+                <Col>
+                    <Form.Control
+                        type="text"
+                        placeholder="Search by username"
+                        value={filterUsername}
+                        onChange={(e) => setFilterUsername(e.target.value)}
+                    />
+                </Col>
+            </Row>
             <div className="gallery-container">
                 <Carousel
                     swipeable={true}
@@ -88,7 +133,7 @@ const PublicGallery = () => {
                     removeArrowOnDeviceType={["tablet", "mobile"]}
                     itemClass="carousel-item-padding-40-px"
                 >
-                    {sharedPhotos.map((photo) => (
+                    {filteredPhotos.map((photo) => (
                         <div key={photo.id} className="photo-container" onClick={() => openImageModal(photo)}>
                             <img
                                 src={photo.url}
@@ -97,7 +142,7 @@ const PublicGallery = () => {
                             />
                             <div className="photo-details">
                                 <h5>{photo.tag}</h5>
-                                <p onClick={() => handleUserNameClick(photo.userId)}>
+                                <p onClick={(e) => handleUserNameClick(photo.userId, e)}>
                                     Uploaded by: {usernames[photo.userId] || 'Unknown'}
                                 </p>
                             </div>
@@ -105,6 +150,31 @@ const PublicGallery = () => {
                     ))}
                 </Carousel>
             </div>
+             {/* User Bio Modal */}
+             <Modal show={showBioModal} onHide={() => setShowBioModal(false)}>
+    <Modal.Header closeButton>
+        <Modal.Title>{selectedUser ? selectedUser.username : ''}</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+        {selectedUser ? (
+            <>
+                <div className="avatar-container"> {/* Open avatar container div */}
+                    {selectedUser.avatar && (
+                        <img 
+                            src={selectedUser.avatar} 
+                            alt="Avatar" 
+                            style={{ width: '150px', height: '150px', objectFit: 'cover', borderRadius: '50%' }} 
+                        />
+                    )}
+                </div> {/* Close avatar container div */}
+                <p>{selectedUser.bio || 'No bio available'}</p>
+            </>
+        ) : (
+            <p>Loading...</p>
+        )}
+    </Modal.Body>
+</Modal>
+             {/* Image Modal */}
             <Modal show={showImageModal} onHide={() => setShowImageModal(false)} size="lg" centered>
                 <Modal.Body>
                     {selectedImage && <img src={selectedImage} alt="Full Size" style={{ width: '100%' }} />}
